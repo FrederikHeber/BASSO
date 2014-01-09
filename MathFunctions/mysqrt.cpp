69db6f4755d9a07d0101cf6e8fe442839aa1fb31
Frederik Heber <heber@math.uni-sb.de>
2014-01-09 16:55:06 +0100

    First part of step 5.

--- MathFunctions/mysqrt.cpp
+++ MathFunctions/mysqrt.cpp
@@ -1,14 +1,19 @@
 #include "MathFunctions.h"
+#include "Table.h"
 
 double mysqrt(const double a)
 {
 	double result = 0.;
 	if (a >= 0) {
+		if (((int)(a) == a) && (a < 10)) {
+			result = sqrtTable[(int)(a)];
+		} else {
 #if defined (HAVE_LOG) && defined (HAVE_EXP)
-		result = exp(log(x) * 0.5);
+			result = exp(log(x) * 0.5);
 # else
-		result = sqrt(a);
+			result = sqrt(a);
 #endif
+		}
 	}
 	return result;
 }
