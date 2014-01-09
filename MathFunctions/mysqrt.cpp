47d8005aa0a6cb2c907689820ddd3f2eb6f313b2
Frederik Heber <heber@math.uni-sb.de>
2014-01-09 16:48:03 +0100

    CMake tutorial step 4.

--- MathFunctions/mysqrt.cpp
+++ MathFunctions/mysqrt.cpp
@@ -3,7 +3,12 @@
 double mysqrt(const double a)
 {
 	double result = 0.;
-	if (a >= 0)
+	if (a >= 0) {
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+		result = exp(log(x) * 0.5);
+# else
 		result = sqrt(a);
+#endif
+	}
 	return result;
 }
