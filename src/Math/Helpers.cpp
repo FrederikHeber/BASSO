357bf4032f5674710732da332e05541b872a77ea
Frederik Heber <heber@math.uni-sb.de>
2014-03-27 17:30:25 +0100

    Added Maths library with Helper functions.
    - is added to EXTRA_LIBS.

---
+++ src/Math/Helpers.cpp
@@ -0,0 +1,52 @@
+/*
+ * Helpers.cpp
+ *
+ *  Created on: Mar 27, 2014
+ *      Author: heber
+ */
+
+#include "BassoConfig.h"
+
+#include "Helpers.hpp"
+
+#include <cmath>
+
+Eigen::VectorXd Helpers::circshift(const Eigen::VectorXd &_x, const int shift)
+{
+	if (shift == 0)
+	return _x;
+	else {
+		// for the moment we just copy the entries and do not use any ...
+		// TODO: fancy mem copying of blocks
+		const int size = _x.innerSize();
+		assert( abs(shift) <= size);
+		Eigen::VectorXd shiftedx(size);
+		for (int i=0;i<size; ++i) {
+			// add size to prevent negative nu8mbers
+			const int index = (i+shift+size)%size;
+			shiftedx[index] = _x[i];
+		}
+		return shiftedx;
+	}
+}
+
+double Helpers::sign(const double _x)
+{
+	if (fabs(_x) < BASSOTOLERANCE)
+		return 0.;
+	else
+		return (_x/fabs(_x));
+}
+
+Eigen::VectorXd Helpers::signum(const Eigen::VectorXd &_x)
+{
+	// signbit returns 1 (negative) or 0 (positive)
+	const int size = _x.innerSize();
+	Eigen::VectorXd signedx(size);
+	for (int i=0;i<size; ++i) {
+		signedx[i] = Helpers::sign(_x[i]);
+	}
+	return signedx;
+}
+
+
---
+++ src/Math/Helpers.hpp
@@ -0,0 +1,42 @@
+/*
+ * Helpers.hpp
+ *
+ *  Created on: Mar 27, 2014
+ *      Author: heber
+ */
+
+#ifndef HELPERS_HPP_
+#define HELPERS_HPP_
+
+#include <Eigen/Dense>
+
+/** Namespace Helpers contains helper functions for linear algebra routines
+ *
+ */
+namespace Helpers {
+	/** Implements a circular shift of the vector's components by \a shift.
+	 *
+	 * Note that the sign of \a shift decides on the direction.
+	 *
+	 * \param shift step-size (and direction) of the shift
+	 * \return VectorXd with shifted components
+	 */
+	Eigen::VectorXd circshift(const Eigen::VectorXd &_x, const int _shift);
+
+	/** Gives the sign (-1/1) of the value \a x.
+	 *
+	 * \param x value
+	 * \return -1, 0, or 1 if \a x is negative, zero or positive
+	 */
+	double sign(const double _x);
+
+	/** Implements signum function, i.e. componentwise sign.
+	 *
+	 * \param x vector
+	 * \return x with -1, 0 or 1 per component
+	 */
+	Eigen::VectorXd signum(const Eigen::VectorXd &_x);
+};
+
+
+#endif /* HELPERS_HPP_ */
