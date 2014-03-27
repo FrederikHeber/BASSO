928ad4e35193d1e438cd450cb7e891ca379c7782
Frederik Heber <heber@math.uni-sb.de>
2014-03-27 13:43:53 +0100

    Testing lp-norms in EigenTest.

--- src/EigenTest.cpp
+++ src/EigenTest.cpp
@@ -6,20 +6,24 @@
  */
 
 #include <iostream>
-#include <Eigen/Core>
-
-using namespace Eigen;
+#include <iomanip>
+#include <Eigen/Dense>
 
 int main()
 {
-	MatrixXd m(2,2);
+	Eigen::MatrixXd m(2,2);
 	m(0,0) = 3;
 	m(1,0) = 2.5;
 	m(0,1) = -1;
 	m(1,1) = m(1,0) + m(0,1);
 	std::cout << "Here is the matrix m:\n" << m << std::endl;
-	VectorXd v(2);
+	Eigen::VectorXd v(2);
 	v(0) = 4;
 	v(1) = v(0) - 1;
 	std::cout << "Here is the vector v:\n" << v << std::endl;
+
+	// testing Norm class
+	std::cout << "Norms of v, L2: " << v.norm()
+			<< ", L1: " << v.lpNorm<1>()
+			<< ", l_infty: " << v.lpNorm<Eigen::Infinity>() << std::endl;
 }
