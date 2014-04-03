5823e4e5c62584f5da92018a73facf6d054a55e4
Frederik Heber <heber@math.uni-sb.de>
2014-04-03 15:32:45 +0200

    Added specific exceptions thrown in libMinimizations.

---
+++ src/Minimizations/MinimizationExceptions.hpp
@@ -0,0 +1,40 @@
+/*
+ * MinimizationExceptions.hpp
+ *
+ *  Created on: Apr 3, 2014
+ *      Author: heber
+ */
+
+#ifndef MINIMIZATIONEXCEPTIONS_HPP_
+#define MINIMIZATIONEXCEPTIONS_HPP_
+
+#include <boost/exception/all.hpp>
+#include <string>
+
+//!> typedef for error information, here a string with the variable name
+typedef boost::error_info<
+		struct tag_MinimizationIllegalValue_name,
+		std::string> MinimizationIllegalValue_name;
+
+//!> typedef for error information, here a function value (double)
+typedef boost::error_info<
+		struct tag_MinimizationFunctionError_name,
+		double> MinimizationFunctionError_name;
+
+/** Exception structure for illegal values handed to a routine.
+ *
+ */
+struct MinimizationIllegalValue_exception :
+	virtual boost::exception,
+	virtual std::exception
+{ };
+
+/** Exception structure on function evaluation.
+ *
+ */
+struct MinimizationFunctionError_exception :
+	virtual boost::exception,
+	virtual std::exception
+{ };
+
+#endif /* MINIMIZATIONEXCEPTIONS_HPP_ */
