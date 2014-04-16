89e8cd4ac76bf86500dce35be8b9db9f96996d27
Frederik Heber <heber@math.uni-sb.de>
2014-04-16 15:39:06 +0200

    We may now use approximate line search alternatively.
    - define USE_APPROXIMATE_LINESEARCH chooses between brent_find_minima from
      boost' mathtools or levmar's function minimization.

--- src/Minimizations/LandweberMinimizer_calculateOptimalStepwidth.cpp
+++ src/Minimizations/LandweberMinimizer_calculateOptimalStepwidth.cpp
@@ -7,8 +7,15 @@
 
 #include "LandweberMinimizer.hpp"
 
+#define USE_APPROXIMATE_LINESEARCH
+
 #include <Eigen/Dense>
+#ifndef USE_APPROXIMATE_LINESEARCH
 #include <levmar.h>
+#else
+#include <boost/bind.hpp>
+#include <boost/math/tools/minima.hpp>
+#endif
 #include <limits>
 
 #include "MinimizationExceptions.hpp"
@@ -37,6 +44,7 @@ struct MinimizationParameters
 	const LandweberMinimizer &landweber;
 };
 
+#ifndef USE_APPROXIMATE_LINESEARCH
 /** Static function to calculate residual for given step width
  *
  */
@@ -60,6 +68,45 @@ func_residual(double *p, double *hx, int m, int n, void *adata)
 			residual
 			);
 }
+#endif
+
+class function_residual
+{
+public:
+	function_residual(
+			const Eigen::VectorXd &_x,
+			const Eigen::VectorXd &_u,
+			const Eigen::MatrixXd &_A,
+			const Eigen::VectorXd &_y,
+			const LandweberMinimizer &_landweber
+			) :
+				x(_x),
+				u(_u),
+				A(_A),
+				y(_y),
+				landweber(_landweber)
+	{}
+	~function_residual() {}
+
+	double operator()(double _arg) const
+	{
+		Eigen::VectorXd dual_solution =
+				landweber.J_p(x, landweber.PowerX);
+		dual_solution -= _arg * u;
+		Eigen::VectorXd x =
+				landweber.J_q(dual_solution, landweber.DualPowerX);
+		// calculate residual at candidate position (goes into hx[0])
+		Eigen::VectorXd residual;
+		return landweber.calculateResidual( x, A, y, residual );
+	}
+
+private:
+	const Eigen::VectorXd &x;
+	const Eigen::VectorXd &u;
+	const Eigen::MatrixXd &A;
+	const Eigen::VectorXd &y;
+	const LandweberMinimizer &landweber;
+};
 
 double LandweberMinimizer::calculateOptimalStepwidth(
 		const Eigen::VectorXd &_x,
@@ -69,6 +116,7 @@ double LandweberMinimizer::calculateOptimalStepwidth(
 		const double _alpha) const
 {
 	double alpha = _alpha;
+#ifndef USE_APPROXIMATE_LINESEARCH
 	struct MinimizationParameters params(
 			_x,	// x_n
 			_u, // u_n
@@ -101,11 +149,37 @@ double LandweberMinimizer::calculateOptimalStepwidth(
 			covar,
 			&params
 			);
-	  if (ret == -1)
+	  if (ret == -1) {
+		alpha = _alpha;
 		throw MinimizationFunctionError_exception()
 			<< MinimizationFunctionError_name(alpha);
+	  }
 	// free everything
 	free(work);
+#else
+	function_residual res(
+			_x,	// x_n
+			_u, // u_n
+			_A, // A
+			_y, // y
+			*this // landweber
+			);
+	double minval = -1000.;
+	double maxval = 1000.;
+	int bits = 32;
+	boost::uintmax_t maxiter = 100;
+	std::pair<double, double> minpair =
+			boost::math::tools::brent_find_minima(
+					boost::bind(
+							&function_residual::operator(),
+							boost::cref(res),
+							_1),
+					minval,
+					maxval,
+					bits,
+					maxiter);
+	alpha = minpair.first;
+#endif
 
 	return alpha;
 }
