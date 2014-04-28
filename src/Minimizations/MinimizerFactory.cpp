2d5558d9e0689ca7b1f69420f13cb5ef6dfd28ff
Frederik Heber <heber@math.uni-sb.de>
2014-04-28 17:42:39 +0200

    Added MinimizerFactory that instantiates specific GeneralMinimizers.

---
+++ src/Minimizations/MinimizerFactory.cpp
@@ -0,0 +1,96 @@
+/*
+ * MinimizerFactory.cpp
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: heber
+ */
+
+#include "BassoConfig.h"
+
+#include "Minimizations/GeneralMinimizer.hpp"
+#include "Minimizations/LandweberMinimizer.hpp"
+#include "Minimizations/MinimizerFactory.hpp"
+#include "Minimizations/SequentialSubspaceMinimizer.hpp"
+
+
+// static entities
+const std::string MinimizerFactory::TypeNames[] = {
+		"landweber",
+		"sequentialsubspace"
+};
+
+MinimizerFactory::instance_ptr_t
+MinimizerFactory::getInstance(
+		const enum InstanceType &_type,
+		const double _NormX,
+		const double _NormY,
+		const double _PowerX,
+		const double _PowerY,
+		const double _Delta,
+		const double _C,
+		const unsigned int _maxiter,
+		const unsigned int _outputsteps
+		)
+{
+	// create the instance depending on the type
+	GeneralMinimizer *instance = NULL;
+	switch(_type) {
+	case landweber:
+		instance = new LandweberMinimizer(
+				_NormX,
+				_NormY,
+				_PowerX,
+				_PowerY,
+				_Delta,
+				_C,
+				_maxiter,
+				_outputsteps
+				);
+		break;
+	case sequentialsubspace:
+			instance = new SequentialSubspaceMinimizer(
+					_NormX,
+					_NormY,
+					_PowerX,
+					_PowerY,
+					_Delta,
+					_C,
+					_maxiter,
+					_outputsteps
+					);
+			break;
+	default:
+		std::cerr << "Illegal or unknown type of GeneralMinimizer requested."
+			<< std::endl;
+		break;
+	}
+
+	// return the wrapped instance
+	return instance_ptr_t(instance);
+}
+
+unsigned int MinimizerFactory::getTypeNamesIndex(
+		const std::string &_name) const
+{
+	unsigned int i=0;
+	for (; i<MAX_InstanceType; ++i)
+		if (TypeNames[i] == _name)
+			break;
+
+	return i;
+}
+
+MinimizerFactory::InstanceType MinimizerFactory::getTypeForName(
+		const std::string &_name) const
+{
+	unsigned int i=getTypeNamesIndex(_name);
+	assert(i != MAX_InstanceType);
+	return (enum InstanceType)i;
+}
+
+const std::string& MinimizerFactory::getNameForType(
+		const enum InstanceType &_type) const
+{
+	assert( (_type >= landweber) && (_type < MAX_InstanceType) );
+	return TypeNames[_type];
+}
---
+++ src/Minimizations/MinimizerFactory.hpp
@@ -0,0 +1,110 @@
+/*
+ * MinimizerFactory.hpp
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: heber
+ */
+
+#ifndef MINIMIZERFACTORY_HPP_
+#define MINIMIZERFACTORY_HPP_
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+
+class GeneralMinimizer;
+
+/** This class is a factory for GeneralMinimizers, producing
+ * specific instances but handing back only the general interface
+ * such that they can be used in the same way.
+ *
+ */
+class MinimizerFactory
+{
+public:
+	typedef boost::shared_ptr<GeneralMinimizer> instance_ptr_t;
+
+	MinimizerFactory() {}
+	~MinimizerFactory() {}
+
+	//!> enumeration of all known instances
+	enum InstanceType {
+		landweber=0,
+		sequentialsubspace=1,
+		MAX_InstanceType
+	};
+
+	/** Produces the desired instance.
+	 *
+	 * @param _type type of instance
+	 * @param _NormX lp norm of source space
+	 * @param _NormY lp norm of target space
+	 * @param _PowerX power type of gauge function of duality mapping in source
+	 * 			space
+	 * @param _PowerY power type of gauge function of duality mapping in target
+	 * 			space
+	 * @param _Delta noise level
+	 * @param _C dampening coefficient
+	 * @param _maxiter maximum number of iterations
+	 * @param _outputsteps write temporary solution each .. steps
+	 * @return wrapped instance of desired \a _type
+	 */
+	instance_ptr_t getInstance(
+			const enum InstanceType &_type,
+			const double _NormX,
+			const double _NormY,
+			const double _PowerX,
+			const double _PowerY,
+			const double _Delta,
+			const double _C,
+			const unsigned int _maxiter,
+			const unsigned int _outputsteps=0
+			);
+
+	/** Helper function to check whether the given \a _name designates a
+	 * known type.
+	 *
+	 * @param _name name of InstanceType to check
+	 * \return true - name ok, false - name unknown
+	 */
+	bool isValidTypeName(
+			const std::string &_name) const
+	{
+		return getTypeNamesIndex(_name) != MAX_InstanceType;
+	}
+
+
+	/** Helper function to resolve internal type id for a given type name.
+	 *
+	 * @param _name name of instance type
+	 * @return id of this instance type
+	 */
+	enum InstanceType getTypeForName(
+			const std::string &_name) const;
+
+	/** Helper function to get name to an internal instance type.
+	 *
+	 * In contrast to direct access to TypeNames we also check that
+	 * _type is valid.
+	 *
+	 * @param _type id of instance type
+	 * @return string containing type's name
+	 */
+	const std::string& getNameForType(
+			const enum InstanceType &_type) const;
+
+protected:
+	/** Internal function to look up the index for the given name.
+	 *
+	 * @param _name name of InstanceType to look up in TypeNames
+	 * @return index in array
+	 */
+	unsigned int getTypeNamesIndex(
+			const std::string &_name) const;
+
+public:
+	//!> name of each known instance type
+	static const std::string TypeNames[];
+};
+
+
+#endif /* MINIMIZERFACTORY_HPP_ */
