04b7ef34b712c18f07f56e4344db38788c63d1f9
Frederik Heber <heber@math.uni-sb.de>
2014-10-24 15:26:20 +0200

    Adding DualityMapping as general Mapping.

---
+++ src/Minimizations/Mappings/DualityMapping.hpp
@@ -0,0 +1,30 @@
+/*
+ * DualityMapping.hpp
+ *
+ *  Created on: Oct 24, 2014
+ *      Author: heber
+ */
+
+#ifndef DUALITYMAPPING_HPP_
+#define DUALITYMAPPING_HPP_
+
+#include "BassoConfig.h"
+
+#include "Minimizations/Mappings/Mapping.hpp"
+#include "Minimizations/Spaces/NormedSpace.hpp"
+
+/** This class defines mappings from a space to its dual space with
+ * elements being the gradient of the norm of the source space.
+ */
+class DualityMapping : public Mapping
+{
+public:
+	DualityMapping(
+			const NormedSpace_ptr_t &_NormedSpaceRef
+			) :
+		Mapping(_NormedSpaceRef,_NormedSpaceRef.getDualSpace())
+	{}
+};
+
+
+#endif /* DUALITYMAPPING_HPP_ */
