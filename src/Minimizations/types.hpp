353286971423bf7760a4adf70c49d2a95f8cc1da
Frederik Heber <heber@math.uni-sb.de>
2014-10-24 15:24:07 +0200

    We use boost::shared_ptr for all new instances, (to be) contained in types.hpp
    - if we only were using C++11 ... with perfect forwarding and move local
      variables could simply be transported outside of the local scope.

---
+++ src/Minimizations/types.hpp
@@ -0,0 +1,17 @@
+/*
+ * types.hpp
+ *
+ *  Created on: Oct 24, 2014
+ *      Author: heber
+ */
+
+#ifndef TYPES_HPP_
+#define TYPES_HPP_
+
+#include "BassoConfig.h"
+
+#include <boost/shared_ptr.hpp>
+
+
+
+#endif /* TYPES_HPP_ */
