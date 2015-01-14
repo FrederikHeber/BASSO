5c1152b5c43054714f916948db8c0ea8a276da56
Frederik Heber <heber@math.uni-sb.de>
2015-01-14 19:20:48 +0100

    Added second projection onto final intersection of hyperspaces.

--- src/Minimizations/Minimizers/SequentialSubspaceMinimizerNoise.cpp
+++ src/Minimizations/Minimizers/SequentialSubspaceMinimizerNoise.cpp
@@ -9,6 +9,7 @@
 
 #include "SequentialSubspaceMinimizerNoise.hpp"
 
+#include <algorithm>
 #include <boost/log/trivial.hpp>
 #include <cmath>
 #include <Eigen/Dense>
@@ -49,7 +50,10 @@ SequentialSubspaceMinimizerNoise::SequentialSubspaceMinimizerNoise(
 		_outputsteps
 		),
 	tau(1.1)
-{}
+{
+	// change y tolerance according to regularization parameter
+	const_cast<double&>(TolY) = tau * Delta;
+}
 
 void SequentialSubspaceMinimizerNoise::setTau(
 		const double _tau
@@ -75,10 +79,14 @@ SequentialSubspaceMinimizerNoise::operator()(
 	boost::chrono::high_resolution_clock::time_point timing_start =
 			boost::chrono::high_resolution_clock::now();
 
+	// make sure we have two search directions
+	// TODO: Remove this when scheme allows for any number of search directions
+	assert ( N == 2);
+
 	// gather some refs for convenient access
 	QuickAccessReferences refs(_problem);
 
-	// G constant used in theoretical step width
+	// G constant
 	const double G =
 			refs.NormX.getPvalue() > 2. ?
 			::pow(2., 2. - refs.DualNormX.getPvalue()) :
@@ -86,7 +94,7 @@ SequentialSubspaceMinimizerNoise::operator()(
 	BOOST_LOG_TRIVIAL(debug)
 		<< "G is " << G;
 
-	// reset inner state if problem has changed
+	/// initialize return structure
 	if (!istate.getisInitialized()) {
 		SpaceElement_ptr_t residual = refs.SpaceY.createElement();
 		const double residuum = calculateResidual(
@@ -99,7 +107,7 @@ SequentialSubspaceMinimizerNoise::operator()(
 				N);
 	}
 
-	// calculate some values prior to loop
+	/// calculate some values prior to loop
 	// Jx=DualityMapping(x,NormX,PowerX,TolX);
 	SpaceElement_ptr_t dual_solution = refs.DualSpaceX.createElement();
 	*dual_solution = _dualstartvalue;
@@ -126,128 +134,145 @@ SequentialSubspaceMinimizerNoise::operator()(
 	const double initial_residuum = istate.residuum;
 	StopCriterion = CheckResiduum(initial_residuum);
 
+	std::vector<double> d(N,0.);
 	while (!StopCriterion) {
+		/// Calculation of search direction
+		searchdir.update(refs, istate.m_residual);
+
+		/// output prior to iterate update
+		istate.output(ynorm);
+
+		// uNorm=norm(u,DualNormX);
+		const double uNorm = refs.DualNormX(searchdir.u);
+		BOOST_LOG_TRIVIAL(trace) << "uNorm is " << uNorm;
+		// alpha=u'*x-Residual^PowerY; (29) or (24) in original form
+		const double alpha =
+				searchdir.u * istate.m_solution - ::pow(istate.residuum, refs.j_r.getPower());
+		BOOST_LOG_TRIVIAL(trace) << "alpha is " << alpha;
+		updateSearchspace(_truesolution, dual_solution, searchdir.u, alpha);
+		const unsigned int index = istate.searchspace->getIndex();
+
+		// d=Delta*Residual^(PowerY-1); (24) or (32)
+		d[index] = Delta * ::pow(istate.residuum, refs.j_r.getPower()-1.);
+		// beta=Residual^(PowerY-1)*(Residual-Delta)/uNorm^DualPowerX;
+		const double beta =
+				::pow(istate.residuum, refs.j_r.getPower()-1.)
+				 *(istate.residuum-Delta)/::pow(uNorm, refs.J_q.getPower());
+
+		/// database update prior to iterate update
 		per_iteration_tuple.replace( "iteration", (int)istate.NumberOuterIterations);
-		if ((istate.NumberOuterIterations == 0)
-			|| (istate.residuum > TolY)) {
-			/// Calculation of search direction
-			searchdir.update(refs, istate.m_residual);
-
-			/// output prior to iterate update
-			istate.output(ynorm);
-
-			/// update search space with new direction
-			// uNorm=norm(u,DualNormX);
-			const double uNorm = refs.DualNormX(searchdir.u);
-			BOOST_LOG_TRIVIAL(debug)
-				<< "uNorm is " << uNorm;
-			// alpha=u'*x-Residual^PowerY;
-			const double alpha =
-					searchdir.u * istate.m_solution - ::pow(istate.residuum,refs.j_r.getPower());
-			BOOST_LOG_TRIVIAL(trace)
-				<< "alpha is " << alpha;
-			updateSearchspace(_truesolution, dual_solution, searchdir.u, alpha);
-
-			// d=Delta*Residual^(PowerY-1);
-			const double d =
-					Delta * ::pow(istate.residuum,(double)refs.j_r.getPower()-1.);
-			// beta=Residual^(PowerY-1)*(Residual-Delta)/uNorm^DualPowerX;
-			const double beta =
-					::pow(istate.residuum,(double)refs.j_r.getPower()-1.)
-					* (istate.residuum-Delta)/::pow(uNorm, refs.J_q.getPower());
-
-			/// database update prior to iterate update
-			per_iteration_tuple.replace( "iteration", (int)istate.NumberOuterIterations);
-			per_iteration_tuple.replace( "residual", istate.residuum);
-			per_iteration_tuple.replace( "relative_residual", istate.residuum/ynorm);
-			per_iteration_tuple.replace( "bregman_distance",
-					calculateBregmanDistance(
-							Delta_p, istate.m_solution, _truesolution, dual_solution));
-			per_iteration_tuple.replace( "error",
-					calculateError(istate.m_solution, _truesolution));
-			per_iteration_tuple.replace( "updated_index", (int)istate.searchspace->getIndex());
+		per_iteration_tuple.replace( "residual", istate.residuum);
+		per_iteration_tuple.replace( "relative_residual", istate.residuum/ynorm);
+		per_iteration_tuple.replace( "bregman_distance",
+				calculateBregmanDistance(
+								Delta_p, istate.m_solution, _truesolution, dual_solution));
+		per_iteration_tuple.replace( "error",
+				calculateError(istate.m_solution, _truesolution));
+		per_iteration_tuple.replace( "updated_index", (int)istate.searchspace->getIndex());
 //			per_iteration_tuple.replace("inner_iterations",
 //					(int) (inner_iterations));
 //			per_iteration_tuple.replace( "stepwidth", sqrt(stepwidth_norm));
-
-			std::vector<double> tmin(1, 0.);
-			if (dual_solution->isApproxToConstant(0, TolX)) {
-				// tmin=beta^(PowerX-1);
-				tmin[0] = ::pow(beta, refs.J_p.getPower() - 1.);
-				BOOST_LOG_TRIVIAL(debug)
-					<< "tmin is " << tmin[0];
-			} else {
-				// t0=(beta/G)^(PowerX-1);
-				std::vector<double> t0(1, 0.);
-				t0[0] = ::pow(beta/G, refs.J_p.getPower() - 1.);
-				BOOST_LOG_TRIVIAL(trace)
-					<< "t0[0] is " << t0[0];
-				{
-					// tmin=fminunc(@(t) BregmanProjectionFunctional(t,Jx,u,alpha+d,DualNormX,DualPowerX,TolX),t0,BregmanOptions);
-					BregmanProjectionFunctional bregman(
-							refs.DualNormX,
-							dynamic_cast<const PowerTypeDualityMapping &>(refs.J_q),
-							refs.J_q.getPower());
-
-					const HyperplaneProjection functional(
-							bregman,
-							dual_solution,
-							istate.getSearchSpace(),
-							istate.getAlphas());
-					Minimizer<gsl_vector> minimizer(1);
-
-					// TODO: current alpha needs to be modified for minimization!
-					std::vector<double> steps(1);
-					steps[0] = alpha+d;
-					FunctionalMinimizer<std::vector<double>, gsl_vector> fmin(
-						functional, minimizer, t0);
-
-					tmin = t0;
-					const unsigned int iterations =
-							fmin(1, TolY, tmin);
-
-					std::stringstream output_stepwidth;
-					std::copy(tmin.begin(), tmin.end(), std::ostream_iterator<double>(output_stepwidth, " "));
-					BOOST_LOG_TRIVIAL(debug)
-						<< "tmin " << output_stepwidth.str()
-						<< " found in " << iterations << " iterations.";
-				}
-			}
-			double stepwidth_norm = 0.;
-			stepwidth_norm = std::inner_product(tmin.begin(), tmin.end(), tmin.begin(), stepwidth_norm);
-			per_iteration_tuple.replace( "stepwidth", sqrt(stepwidth_norm));
-			// x=DualityMapping(Jx-tmin*u,DualNormX,DualPowerX,TolX);
-			/// update iterate
-			updateIterates(refs, tmin, _problem->x, dual_solution);
-
-			/// update residual
-			istate.residuum = calculateResidual(
-					_problem,
-					istate.m_residual);
-
-			/// check iterations count/wall time
-			boost::chrono::high_resolution_clock::time_point timing_intermediate =
-					boost::chrono::high_resolution_clock::now();
-			++istate.NumberOuterIterations;
-			const double current_residuum = istate.residuum;
-			StopCriterion =
-					CheckIterations(istate.NumberOuterIterations)
-					|| CheckResiduum(current_residuum)
-					|| CheckWalltime(boost::chrono::duration<double>(timing_intermediate - timing_start));
-
-			/// check for non-convergence
-			if (isNonConverging(current_residuum,
-					initial_residuum))
-				fillPerIterationTable(per_iteration_tuple, per_iteration_table);
-
-			/// print intermediate solution
-			printIntermediateSolution(
-					istate.m_solution, refs.A, istate.NumberOuterIterations);
-		}
-
-
-		/// submit current tuple
+		// submit current tuples
 		per_iteration_table.addTuple(per_iteration_tuple);
+
+		/// 1st update onto new hyperplane
+		// index contains newest direction on whose associated hyperplane
+		// x in general does not lie.
+		double stepwidth_norm = 0.;
+		std::vector<double> tmin(1, 0.);
+		if (dual_solution->isApproxToConstant(0, TolX)) {
+			// tmin=beta^(PowerX-1);
+			tmin[0] = ::pow(beta, refs.J_p.getPower() - 1.);
+			BOOST_LOG_TRIVIAL(trace)
+				<< "tmin is " << tmin[0];
+		} else {
+			// t0=(beta/G)^(PowerX-1);
+			// wrong, rather we have
+			// t0=(Residual*(Residual-Delta)/(G*uNorm^DualPowerX))^(PowerX-1);
+			// see Remark b) on page 17
+			std::vector<double> t0(1, 0.);
+			tmin[0] = ::pow(beta/G, refs.J_p.getPower() - 1.);
+			BOOST_LOG_TRIVIAL(trace)
+				<< "Initial tmin[0] is " << t0[0];
+
+			std::vector<SpaceElement_ptr_t> ConstrainedSearchSpace(1);
+			ConstrainedSearchSpace[0] = istate.getSearchSpace()[index];
+			std::vector<double> steps(1);
+			steps[0] = istate.getAlphas()[index]+d[index];
+
+			const unsigned int inner_iterations =
+					calculateStepWidth(refs, dual_solution, tmin,
+							ConstrainedSearchSpace, steps);
+		}
+		stepwidth_norm = std::inner_product(tmin.begin(), tmin.end(), tmin.begin(), stepwidth_norm);
+		// x=DualityMapping(Jx-tmin*u,DualNormX,DualPowerX,TolX);
+		/// update iterate
+		tmin.resize(2, 0.);
+		if (index != 0)
+			std::swap(tmin[index], tmin[0]);
+		updateIterates(refs, tmin, _problem->x, dual_solution);
+
+		if (istate.NumberOuterIterations > 0) {
+			/// 2nd update onto intersection of hyperspaces
+			// find "old" index (one before current index)
+			const std::vector<unsigned int> &lastIndices = istate.searchspace->getLastIndices();
+			const std::vector<unsigned int>::const_iterator iter =
+					std::find( lastIndices.begin(), lastIndices.end(), (unsigned int)1);
+			// with N == 2 we must always
+			assert ( iter != lastIndices.end() );
+			const unsigned int lastIndex =
+					std::distance( lastIndices.begin(), iter );
+			const double Resold = istate.getSearchSpace()[lastIndex] * istate.m_solution;
+
+			// project onto correct second hyperplane intersection
+			std::vector<double> steps(N);
+			steps[index] = istate.getAlphas()[index]+d[index];
+			steps[lastIndex] = istate.getAlphas()[lastIndex];
+			if ((Resold > steps[lastIndex]+d[lastIndex]+BASSOTOLERANCE)
+					|| (Resold < steps[lastIndex]-d[lastIndex]-BASSOTOLERANCE)) {
+				if (Resold > steps[lastIndex]+d[lastIndex]+BASSOTOLERANCE) {
+					steps[lastIndex] = steps[lastIndex]+d[lastIndex];
+				} else {
+					steps[lastIndex] = steps[lastIndex]-d[lastIndex];
+				}
+
+				// only update a second time if we are not in intersection already
+				std::vector<double> tmin(2, 0.);
+				const unsigned int inner_iterations =
+						calculateStepWidth(refs, dual_solution, tmin,
+								istate.getSearchSpace(), steps);
+
+				/// update iterate
+				updateIterates(refs, tmin, _problem->x, dual_solution);
+			}
+		}
+		stepwidth_norm = std::inner_product(tmin.begin(), tmin.end(), tmin.begin(), stepwidth_norm);
+		per_iteration_tuple.replace( "stepwidth", sqrt(stepwidth_norm));
+
+		/// submit current tuples
+		per_iteration_table.addTuple(per_iteration_tuple);
+
+		/// update residual
+		istate.residuum = calculateResidual(_problem,istate.m_residual);
+
+		/// check iterations count/wall time
+		boost::chrono::high_resolution_clock::time_point timing_intermediate =
+				boost::chrono::high_resolution_clock::now();
+		++istate.NumberOuterIterations;
+		const double current_residuum = istate.residuum;
+		StopCriterion =
+				CheckIterations(istate.NumberOuterIterations)
+				|| CheckResiduum(current_residuum)
+				|| CheckWalltime(boost::chrono::duration<double>(timing_intermediate - timing_start));
+
+		/// check for non-convergence
+		if (isNonConverging(current_residuum,
+				initial_residuum))
+			fillPerIterationTable(per_iteration_tuple, per_iteration_table);
+
+		/// print intermediate solution
+		printIntermediateSolution(
+				istate.m_solution, refs.A, istate.NumberOuterIterations);
 	}
 
 	boost::chrono::high_resolution_clock::time_point timing_end =
