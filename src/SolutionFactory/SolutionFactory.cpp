4b72341b9f843e14ef647cf61112d47537c67472
Frederik Heber <heber@math.uni-sb.de>
2015-01-28 17:37:36 +0100

    Added SolutionFactory containing static helper functions to hoist code out of Basso.
    - this is preparatory to avoid code duplication in MatrixFactorizer.

---
+++ src/SolutionFactory/SolutionFactory.cpp
@@ -0,0 +1,142 @@
+/*
+ * SolutionFactory.cpp
+ *
+ *  Created on: Jan 28, 2015
+ *      Author: heber
+ */
+
+
+#include "BassoConfig.h"
+
+#include "SolutionFactory.hpp"
+
+#include "CommandLineOptions/CommandLineOptions.hpp"
+#include "Database/Database.hpp"
+#include "Log/Logging.hpp"
+#include "Minimizations/InverseProblems/InverseProblem.hpp"
+#include "Minimizations/InverseProblems/InverseProblemFactory.hpp"
+#include "Minimizations/Minimizers/MinimizationExceptions.hpp"
+#include "Minimizations/Minimizers/GeneralMinimizer.hpp"
+#include "Minimizations/Minimizers/LandweberMinimizer.hpp"
+#include "Minimizations/Minimizers/SequentialSubspaceMinimizer.hpp"
+#include "Minimizations/Minimizers/SequentialSubspaceMinimizerNoise.hpp"
+#include "Minimizations/Minimizers/StepWidths/DetermineStepWidthFactory.hpp"
+
+InverseProblem_ptr_t SolutionFactory::createInverseProblem(
+		const CommandLineOptions &_opts,
+		const Eigen::MatrixXd &_matrix,
+		const Eigen::VectorXd &_rhs)
+{
+	// prepare inverse problem
+	InverseProblem_ptr_t inverseproblem;
+	switch (_opts.dualitytype) {
+	case CommandLineOptions::regularizedl1norm:
+		inverseproblem = InverseProblemFactory::createRegularizedL1Instance(
+				_opts.regularization_parameter,
+				_opts.powerx,
+				_opts.normy,
+				_opts.powery,
+				_matrix,
+				_rhs);
+		break;
+	case CommandLineOptions::defaulttype:
+	default:
+		inverseproblem = InverseProblemFactory::createLpInstance(
+				_opts.normx,
+				_opts.powerx,
+				_opts.normy,
+				_opts.powery,
+				_matrix,
+				_rhs);
+		break;
+	}
+
+	return inverseproblem;
+}
+
+Database_ptr_t SolutionFactory::createDatabase(
+		const CommandLineOptions &_opts
+		)
+{
+	Database_ptr_t database(new Database());
+	if (!_opts.iteration_file.string().empty())
+		database->setDatabaseFile(_opts.iteration_file.string());
+	database->setReplacePresentParameterTuples(_opts.database_replace);
+
+	return database;
+}
+
+MinimizerFactory::instance_ptr_t SolutionFactory::createMinimizer(
+		const CommandLineOptions &_opts,
+		InverseProblem_ptr_t &_inverseproblem,
+		Database_ptr_t &_database,
+		const unsigned int _maxiterations
+		)
+{
+	MinimizerFactory factory;
+	MinimizerFactory::instance_ptr_t minimizer;
+	// set regularization parameter in case of regularizedl1norm
+	minimizer =
+		factory.createInstance(
+			_opts.type,
+			_inverseproblem,
+			_opts.delta,
+			_maxiterations,
+			_opts.maxinneriter,
+			*_database,
+			(const enum DetermineStepWidthFactory::stepwidth_enumeration)_opts.stepwidth_type,
+			_opts.outputsteps);
+	minimizer->setMinLib(_opts.minlib);
+	// hand over additional parameters to add to submitted tuples
+	minimizer->setAdditionalTupleParameters(_opts.tuple_parameters);
+
+	try {
+		// create instance with some specifics
+		switch(_opts.type) {
+		case MinimizerFactory::landweber:
+			static_cast<LandweberMinimizer*>(
+					minimizer.get())->setC(_opts.C);
+			break;
+		case MinimizerFactory::sequentialsubspace:
+			static_cast<SequentialSubspaceMinimizer*>(minimizer.get())->setN(_opts.N);
+			static_cast<SequentialSubspaceMinimizer*>(
+					minimizer.get())->setupdateIndexAlgorithm(
+							_opts.updatetype);
+			static_cast<SequentialSubspaceMinimizer*>(
+					minimizer.get())->setEnforceRandomMapping(
+							_opts.enforceRandomMapping);
+			static_cast<SequentialSubspaceMinimizer*>(minimizer.get())->setInexactLinesearch(
+					_opts.inexactLinesearch);
+			if (_opts.wolfe_constants.size() == 2) {
+				static_cast<SequentialSubspaceMinimizer*>(minimizer.get())->setWolfeConstants(
+						_opts.wolfe_constants);
+				// warning in case sanity check fails
+				if (!_opts.inexactLinesearch)
+					BOOST_LOG_TRIVIAL(warning)
+						<< "Wolfe constants set although we do perform an exact line search.";
+			}
+			static_cast<SequentialSubspaceMinimizer*>(
+					minimizer.get())->setDoCalculateAngles(
+							_opts.calculateAngles);
+			break;
+		case MinimizerFactory::sequentialsubspace_noise:
+			static_cast<SequentialSubspaceMinimizerNoise*>(
+					minimizer.get())->setTau(_opts.tau);
+			static_cast<SequentialSubspaceMinimizerNoise*>(
+					minimizer.get())->setN(_opts.N);
+			break;
+		default:
+			std::cerr << "Unknown InstanceType"
+				<< MinimizerFactory::getNameForType(_opts.type) << "." << std::endl;
+			return MinimizerFactory::instance_ptr_t();
+			break;
+		}
+	} catch (MinimizationIllegalValue_exception &e) {
+		std::cerr << "Illegal value for "
+				<< *boost::get_error_info<MinimizationIllegalValue_name>(e)
+				<< std::endl;
+		return MinimizerFactory::instance_ptr_t();
+	}
+
+	return minimizer;
+}
---
+++ src/SolutionFactory/SolutionFactory.hpp
@@ -0,0 +1,74 @@
+/*
+ * SolutionFactory.hpp
+ *
+ *  Created on: Jan 28, 2015
+ *      Author: heber
+ */
+
+#ifndef SOLUTIONFACTORY_SOLUTIONFACTORY_HPP_
+#define SOLUTIONFACTORY_SOLUTIONFACTORY_HPP_
+
+#include "BassoConfig.h"
+
+#include <Eigen/Dense>
+
+#include "Minimizations/Minimizers/MinimizerFactory.hpp"
+#include "Minimizations/types.hpp"
+
+class CommandLineOptions;
+
+/** This class contains static functions to ease in building executables
+ * that require solving of inverse problems.
+ *
+ * The difficulty is that all classes in the folder Minimizations/
+ * need many options but the CommandLineOptions/ folder should not be
+ * known as the manner of parsing these options has nothing to do with
+ * the inverse problem solving. Hence, we basically need an adaptor
+ * between the CommandLineOptions and derived classes, wherein all the
+ * required information for setting up and solving the the inverse
+ * problem is contained, and the Minimization stuff, wherein all
+ * functionality is contained for actually solving the problems.
+ */
+struct SolutionFactory
+{
+	/** Creates the inverse problem given the information in \a _opts.
+	 *
+	 * \param _opts CommandLineOptions struct containing all information
+	 * \param  _matrix matrix of the inverse problem
+	 * \param  _rhs right-hand side of the problem
+	 * \return constructed inverse problem instance
+	 */
+	static InverseProblem_ptr_t createInverseProblem(
+			const CommandLineOptions &_opts,
+			const Eigen::MatrixXd &_matrix,
+			const Eigen::VectorXd &_rhs);
+
+	/** Creates a database instance from the information contained in \a _opts.
+	 *
+	 * @param _opts CommandLineOptions struct containing all information
+	 * @return constructed Database instance
+	 */
+	static Database_ptr_t createDatabase(
+			const CommandLineOptions &_opts);
+
+	/** Creates a minimizer instance from the information contained in \a _opts.
+	 *
+	 * @param _opts CommandLineOptions struct containing all information
+	 * @param _inverseproblem inverse problem to be solved/minimized
+	 * @param _database database to write iteration data to
+	 * @param _maxiterations maximum iterations for the minimizer
+	 * @return NULL - something went wrong,
+	 * 		   else - constructed minimizer instance, ready to go
+	 *
+	 */
+	static MinimizerFactory::instance_ptr_t createMinimizer(
+		const CommandLineOptions &_opts,
+		InverseProblem_ptr_t &_inverseproblem,
+		Database_ptr_t &_database,
+		const unsigned int _maxiterations
+		);
+};
+
+
+
+#endif /* SOLUTIONFACTORY_SOLUTIONFACTORY_HPP_ */
