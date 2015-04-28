13bedf406d6bfe922f8819c30bfe4001767e4caf
Frederik Heber <heber@ins.uni-bonn.de>
2015-04-28 12:54:32 +0200

    FIX: DualRegularizedNorm needs an internal l2 norm in the dual space.
    - SoftThresholdingMapping projects given _x into dual space, hence the
      subsequent evaluation needs to be in the dual space. Therefore,
      we must not inherit the l2norm but need it as a distinct internal object.

--- src/Minimizations/Norms/DualRegularizedL1Norm.hpp
+++ src/Minimizations/Norms/DualRegularizedL1Norm.hpp
@@ -16,14 +16,19 @@
 #include "Minimizations/Norms/Norm.hpp"
 #include "Minimizations/Spaces/NormedSpace.hpp"
 
-/** This class implements a regularized l1 norm of the form
- * \f$ \lambda ||.||_1 + \tfrac 1 2 ||.||^2_2 \f$.
+/** This class implements the dual of the regularized l1 norm of the form
+ * \f$ \lambda ||.||_1 + \tfrac 1 2 ||.||^2_2 \f$ which is
+ * \f$ || S_{\lambda} (.) ||_2 \f$ .
  *
  */
-class DualRegularizedL1Norm : public LpNorm
+class DualRegularizedL1Norm : public Norm
 {
 public:
 	/** Constructor for class Norm.
+	 *
+	 * \note The internal SoftThresholdingMapping brings the given \a _x
+	 * into its dual space. Hence, we set the internal L2-norm
+	 * onto this space as well.
 	 *
 	 * @param _ref reference to the space this norm is associated with
 	 * @param _ref reference to the space this norm is associated with
@@ -31,8 +36,9 @@ public:
 	DualRegularizedL1Norm(
 			const NormedSpace_weakptr_t& _ref,
 			const double _lambda = 0.1) :
-		LpNorm(_ref, 2.),
-		softthresholder(_ref, _lambda)
+		Norm(_ref),
+		softthresholder(_ref, _lambda),
+		l2norm(softthresholder.getTargetSpace(), 2.)
 	{}
 
 	/** Setter for soft thresholding parameter \a lambda.
@@ -53,22 +59,24 @@ public:
 
 protected:
 
-	/** Evaluates the norm for a given \a _element.
+	/** Evaluates the norm for a given \a _x.
 	 *
-	 * @param _element element of the space, whose norm to evaluated
-	 * @return norm of \a element
+	 *  @param _x element of the space, whose norm to evaluated
+	 * @return norm of \a _x
 	 */
 	const double internal_operator(const SpaceElement_ptr_t &_x) const
 	{
 		assert( getSpace() == _x->getSpace() );
-		const double value =
-				LpNorm::operator()(softthresholder(_x));
+		const double value = l2norm(softthresholder(_x));
 		return value;
 	}
 
 private:
 	//!> internal soft thresholding operator
 	mutable SoftThresholdingMapping softthresholder;
+
+	//!> internal l2 norm
+	const LpNorm l2norm;
 };
 
 
