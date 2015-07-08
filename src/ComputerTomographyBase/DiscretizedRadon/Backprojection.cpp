b1358d4cf4b0fc1c743fa571be57138a1cc43542
Frederik Heber <heber@math.uni-sb.de>
2015-07-08 22:35:32 +0200

    Added Backprojection to construct backprojection matrix.

---
+++ src/ComputerTomographyBase/DiscretizedRadon/Backprojection.cpp
@@ -0,0 +1,106 @@
+/*
+ * Backprojection.cpp
+ *
+ *  Created on: Jul 8, 2015
+ *      Author: heber
+ */
+
+
+#include "BassoConfig.h"
+
+#include "Backprojection.hpp"
+
+#include <fstream>
+
+#include "Log/Logging.hpp"
+#include "MatrixIO/MatrixIO.hpp"
+
+/** Returns the center of the pixel indicated by the indices \a _i
+ * and \a _j, this is times 1 over h_x/h_y.
+ *
+ * @param _i index for x
+ * @param _j index for y
+ * @param _h_x length of a pixel in x direction
+ * @param _h_y length of a pixel in y direction
+ * @return pixel center coordinates
+ */
+static point_t getPixelCenter(
+		const unsigned int _i,
+		const unsigned int _j,
+		const double _h_x,
+		const double _h_y
+		)
+{
+	point_t point;
+	point[0] = -1.+(_i+.5)*_h_x;
+	point[1] = -1.+(_j+.5)*_h_y;
+	return point;
+}
+
+Backprojection::Backprojection(
+		const unsigned int _num_pixel_x,
+		const unsigned int _num_pixel_y,
+		const unsigned int _num_angles,
+		const unsigned int _num_offsets) :
+		num_pixel_x(_num_pixel_x),
+		num_pixel_y(_num_pixel_y),
+		num_angles(_num_angles),
+		num_offsets(_num_offsets),
+		matrix(_num_pixel_x*_num_pixel_y, _num_angles*_num_offsets)
+{
+	matrix.setZero();
+
+	assert( _num_offsets % 2 == 1);
+	const int half_offsets = (_num_offsets-1) / 2;
+	const double delta = 1./(double)half_offsets;
+	const double h_x = 2./(double)num_pixel_x;
+	const double h_y = 2./(double)num_pixel_y;
+	const double prefactor = h_x; //2.*M_PI/(double)num_angles;
+	for (unsigned int angle = 0; angle < _num_angles; ++angle) {
+		const double phi = angle*M_PI/_num_angles;
+		point_t omega;
+		omega[0] = cos(phi);
+		omega[1] = sin(phi);
+		for (unsigned int pixel_x = 0; pixel_x < num_pixel_x; ++pixel_x) {
+			for (unsigned int pixel_y = 0; pixel_y < num_pixel_y; ++pixel_y) {
+				const unsigned int row_index = pixel_y + (pixel_x*num_pixel_y);
+				const point_t x = getPixelCenter(pixel_x, pixel_y, h_x, h_y);
+				if (x.squaredNorm() > 1.)
+					continue;
+				const double s = omega.dot(x);
+				const double t = s/delta;
+				const int k = floor(t);
+				const double u = t - (double)k;
+				assert( (u > -BASSOTOLERANCE) && (u-1. < BASSOTOLERANCE) );
+				const unsigned int col_index =
+						angle + ((k+half_offsets)*num_angles);
+				const unsigned int next_col_index =
+						col_index + num_angles;	// means k+1
+				matrix( row_index, col_index) = prefactor*(1.-u);
+				matrix( row_index, next_col_index) = prefactor*u;
+			}
+		}
+	}
+}
+
+int Backprojection::load(const boost::filesystem::path &_file)
+{
+	using namespace MatrixIO;
+
+	{
+		std::ifstream ist(_file.string().c_str());
+		if (ist.good())
+			try {
+				ist >> matrix;
+			} catch (MatrixIOStreamEnded_exception &e) {
+				std::cerr << "Failed to fully parse matrix from " << _file.string() << std::endl;
+				return 255;
+			}
+		else {
+			std::cerr << "Failed to open " << _file.string() << std::endl;
+			return 255;
+		}
+	}
+
+	return 0;
+}
---
+++ src/ComputerTomographyBase/DiscretizedRadon/Backprojection.hpp
@@ -0,0 +1,80 @@
+/*
+ * Backprojection.hpp
+ *
+ *  Created on: Jul 8, 2015
+ *      Author: heber
+ */
+
+#ifndef COMPUTERTOMOGRAPHYBASE_DISCRETIZEDRADON_BACKPROJECTION_HPP_
+#define COMPUTERTOMOGRAPHYBASE_DISCRETIZEDRADON_BACKPROJECTION_HPP_
+
+#include "BassoConfig.h"
+
+#include <boost/filesystem/path.hpp>
+
+#include "ComputerTomographyBase/DiscretizedRadon/point_t.hpp"
+
+/** This class implements the back projection which is the adjoint operator
+ * to the Radon transform.
+ */
+class Backprojection
+{
+public:
+	//!> constant for dimension of space
+	enum { dim=2 };
+
+	/** Constructor for class Backprojection.
+	 *
+	 * Dimension of the vector of the right-hand side of the problem is
+	 * \a _num_angles times \a _num_offsets. The dimension of the solution
+	 * vector is \a _num_pixel_x \times \a _num_pixel_y.
+	 *
+	 * @param _num_pixel_x number of pixel in x direction of the image to reconstruct
+	 * @param _num_pixel_y number of pixel in y direction of the image to reconstruct
+	 * @param _num_beams number of beams (angles, offsets) in the measured data
+	 * @param _num_offsets number of beams (angles, offsets) in the measured data
+	 */
+	Backprojection(
+			const unsigned int _num_pixel_x,
+			const unsigned int _num_pixel_y,
+			const unsigned int _num_angles,
+			const unsigned int _num_offsets);
+
+	/** Destructor for class DiscretizedRadonMatrix.
+	 *
+	 */
+	~Backprojection() {}
+
+	/** Loads the matrix from a given \a _file.
+	 *
+	 * @param _file filename to load from
+	 * @return 255 - failure, 0 - good
+	 */
+	int load(const boost::filesystem::path &_file);
+
+	/** Getter for the constructed matrix.
+	 *
+	 * @return const ref to matrix object
+	 */
+	const Eigen::MatrixXd& getMatrix() const
+	{ return matrix; }
+
+private:
+
+private:
+	//!> number of pixel in x direction of the image to reconstruct
+	const unsigned int num_pixel_x;
+	//!> number of pixel in y direction of the image to reconstruct
+	const unsigned int num_pixel_y;
+	//!> number of beams (angles, offsets) in the measured data
+	const unsigned int num_angles;
+	//!> number of beams (angles, offsets) in the measured data
+	const unsigned int num_offsets;
+
+	//!> matrix containing discretized backprojection
+	Eigen::MatrixXd matrix;
+};
+
+
+
+#endif /* COMPUTERTOMOGRAPHYBASE_DISCRETIZEDRADON_BACKPROJECTION_HPP_ */
