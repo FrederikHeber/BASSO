abb58816457eca4ec559d9473afd5692d020108e
Frederik Heber <heber@math.uni-sb.de>
2015-07-23 17:08:52 +0200

    We can store and parse sparse Eigen matrices.

--- src/MatrixIO/MatrixIO.hpp
+++ src/MatrixIO/MatrixIO.hpp
@@ -11,6 +11,7 @@
 #include "BassoConfig.h"
 
 #include <Eigen/Dense>
+#include <Eigen/SparseCore>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -28,7 +29,9 @@ namespace MatrixIO {
  * \param m matrix to write in Matlab-like format
  * \return stream for concatenation
  */
-inline std::ostream & operator<<(std::ostream &ost, const Eigen::MatrixXd &m)
+inline std::ostream & operator<<(
+		std::ostream &ost,
+		const Eigen::MatrixXd &m)
 {
 	if (ost.good()) {
 		const Eigen::MatrixXd::Index rows = m.innerSize();
@@ -39,6 +42,38 @@ inline std::ostream & operator<<(std::ostream &ost, const Eigen::MatrixXd &m)
 			Eigen::MatrixXd::Index col = 0;
 			for (; (col < cols) && ost.good(); ++col )
 				ost << m(row, col) << " ";
+			ost << std::endl;
+			if (col != cols)
+				throw MatrixIOStreamEnded_exception();
+		}
+		if (row != rows)
+			throw MatrixIOStreamEnded_exception();
+	}
+	return ost;
+}
+
+/** Output contents of \a m into stream \ost in Matlab-like format.
+ *
+ * \param ost output stream
+ * \param m matrix to write in Matlab-like format
+ * \return stream for concatenation
+ */
+template <typename type,
+	int options>
+inline std::ostream & operator<<(
+		std::ostream &ost,
+		const Eigen::SparseMatrix<type, options> &m)
+{
+	typedef typename Eigen::SparseMatrix<type, options>::Index index_t;
+	if (ost.good()) {
+		const index_t rows = m.innerSize();
+		const index_t cols = m.outerSize();
+		ost << rows << " " << cols << std::endl;
+		index_t row = 0;
+		for (; (row < rows) && ost.good(); ++row ) {
+			index_t col = 0;
+			for (; (col < cols) && ost.good(); ++col )
+				ost << m.coeff(row, col) << " ";
 			ost << std::endl;
 			if (col != cols)
 				throw MatrixIOStreamEnded_exception();
@@ -76,12 +111,14 @@ inline std::ostream & operator<<(std::ostream &ost, const Eigen::VectorXd &v)
  * @param m matrix to parse
  * @return stream for concatenation
  */
-inline std::istream & operator>>(std::istream &ist, Eigen::MatrixXd &m)
+inline std::istream & operator>>(
+		std::istream &ist,
+		Eigen::MatrixXd &m)
 {
 	if (ist.good()) {
 		// parse first line with dimensions
-		unsigned int rows = 0;
-		unsigned int cols = 0;
+		Eigen::MatrixXd::Index rows = 0;
+		Eigen::MatrixXd::Index cols = 0;
 		{
 			std::string contents;
 			getline(ist, contents);
@@ -92,14 +129,59 @@ inline std::istream & operator>>(std::istream &ist, Eigen::MatrixXd &m)
 		m.resize(rows, cols);
 
 		// parse following lines
-		unsigned int i=0;
+		Eigen::MatrixXd::Index i=0;
 		for (; (i < rows) && ist.good(); ++i) {
 			std::string contents;
 			getline(ist, contents);
 			std::stringstream sstr(contents);
-			unsigned int j=0;
+			Eigen::MatrixXd::Index j=0;
 			for (; (j < cols) && sstr.good(); ++j)
-				sstr >> m(i,j);
+				sstr >> m.coeffRef(i,j);
+			if (j != cols)
+				throw MatrixIOStreamEnded_exception();
+		}
+		if (i != rows)
+			throw MatrixIOStreamEnded_exception();
+	}
+	return ist;
+}
+
+/** Parse a Matlab-like formatted stream \a ist into the matrix \a m.
+ *
+ * @param ist input stream
+ * @param m matrix to parse
+ * @return stream for concatenation
+ */
+template <
+	typename type,
+	int options>
+inline std::istream & operator>>(
+		std::istream &ist,
+		Eigen::SparseMatrix<type, options> &m)
+{
+	typedef typename Eigen::SparseMatrix<type, options>::Index index_t;
+	if (ist.good()) {
+		// parse first line with dimensions
+		index_t rows = 0;
+		index_t cols = 0;
+		{
+			std::string contents;
+			getline(ist, contents);
+			std::stringstream sstr(contents);
+			sstr >> rows;
+			sstr >> cols;
+		}
+		m.resize(rows, cols);
+
+		// parse following lines
+		index_t i=0;
+		for (; (i < rows) && ist.good(); ++i) {
+			std::string contents;
+			getline(ist, contents);
+			std::stringstream sstr(contents);
+			index_t j=0;
+			for (; (j < cols) && sstr.good(); ++j)
+				sstr >> m.coeffRef(i,j);
 			if (j != cols)
 				throw MatrixIOStreamEnded_exception();
 		}
