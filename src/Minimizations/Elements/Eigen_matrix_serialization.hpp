e927220e4a1ad3476f6ba904f579d35c52afb654
Frederik Heber <heber@math.uni-sb.de>
2015-11-18 16:32:17 +0100

    Split non-intrusive Eigen::Matrix serialize() into load()/save().
    - this avoids to have code specific to only one side in both functions.

--- src/Minimizations/Elements/Eigen_matrix_serialization.hpp
+++ src/Minimizations/Elements/Eigen_matrix_serialization.hpp
@@ -13,28 +13,57 @@
 #include <Eigen/Dense>
 
 #include <boost/serialization/array.hpp>
+#include <boost/serialization/split_free.hpp>
 
 /** Answer from
  * http://stackoverflow.com/questions/12851126/serializing-eigens-matrix-using-boost-serialization
  */
 namespace boost
 {
-    template<class Archive, typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
-    inline void serialize(
-        Archive & ar,
-        Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> & t,
-        const unsigned int file_version
-    )
-    {
-        size_t rows = t.rows();
-		size_t cols = t.cols();
-        ar & rows;
-        ar & cols;
-        if ((rows != t.rows()) || (cols != t.cols()))
-        	t.resize( rows, cols );
+	namespace serialization {
+		template<class Archive, typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+		inline void load(
+			Archive & ar,
+			Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> & t,
+			const unsigned int file_version
+		)
+		{
+			size_t rows;
+			size_t cols;
+			ar >> rows;
+			ar >> cols;
+			if ((rows != -1) && (cols != -1)) {
+				if ((rows != t.rows()) || (cols != t.cols()))
+					t.resize( rows, cols );
+				ar >> boost::serialization::make_array(t.data(), t.size());
+			}
+		}
 
-        ar & boost::serialization::make_array(t.data(), t.size());
-    }
-}
+		template<class Archive, typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+		inline void save(
+			Archive & ar,
+			const Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> & t,
+			const unsigned int file_version
+		)
+		{
+			size_t rows = t.rows();
+			size_t cols = t.cols();
+			ar << rows;
+			ar << cols;
+			if ((rows != -1) && (cols != -1))
+				ar << boost::serialization::make_array(t.data(), t.size());
+		}
+
+		template<class Archive, typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+		inline void serialize(
+		    Archive & ar,
+			Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> & t,
+		    const unsigned int file_version
+		){
+		    split_free(ar, t, file_version);
+		}
+
+	} /* namespace serialization */
+} /* namespace boost */
 
 #endif /* MINIMIZATIONS_ELEMENTS_EIGEN_MATRIX_SERIALIZATION_HPP_ */
