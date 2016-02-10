f524c93c9beebb1c2627fdd5b465101fd04b861f
Frederik Heber <heber@math.uni-sb.de>
2016-02-10 17:48:40 +0100

    FIX: executeTestCommand was not looking for test_nr from beginning of line only.
    - i.e. "13/267" would match also "113/267", executing the wrong test.

--- util/executeTestCommand.sh
+++ util/executeTestCommand.sh
@@ -4,4 +4,4 @@ test ! -z $2 || { echo "Usage: $0 <test no.> <total no. of tests>"; exit 255; }
 
 testnr=$1; 
 total=$2;
-command=`grep -m 2 --after-context=1 "${testnr}/${total}" Testing/Temporary/LastTest.log | grep -v "${testnr}/${total}" | sed -e "s/Command: //" | xargs`; $command
+command=`grep -m 2 --after-context=1 "^${testnr}/${total}" Testing/Temporary/LastTest.log | grep -v "${testnr}/${total}" | sed -e "s/Command: //" | xargs`; $command
