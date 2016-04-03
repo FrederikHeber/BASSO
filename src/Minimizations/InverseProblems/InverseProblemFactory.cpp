87df6f6020b44929a8ba54a3a4c7b6de43b5f19b
Frederik Heber <heber@ins.uni-bonn.de>
2016-04-03 17:55:44 +0200

    Added InverseProblemFactory::createFromTwoFactors().
    - this allows to specify an inverse problem given two matrixes whose product is
      the discretized operator to invert.

--- src/Minimizations/InverseProblems/InverseProblemFactory.cpp
+++ src/Minimizations/InverseProblems/InverseProblemFactory.cpp
@@ -10,11 +10,42 @@
 
 #include "InverseProblemFactory.hpp"
 
+#include <boost/bind.hpp>
+
 #include "Minimizations/Elements/ElementCreator.hpp"
 #include "Minimizations/Elements/SpaceElement.hpp"
 #include "Minimizations/Mappings/LinearMappingFactory.hpp"
 #include "Minimizations/InverseProblems/InverseProblem.hpp"
 #include "Minimizations/Spaces/NormedSpaceFactory.hpp"
+
+InverseProblem_ptr_t InverseProblemFactory::create_internal(
+		const std::string &_type_SpaceX,
+		const args_t &_args_SpaceX,
+		const std::string &_type_SpaceY,
+		const args_t &_args_SpaceY,
+		const Eigen::VectorXd &_rhs,
+		const unsigned int _outerSize,
+		const unsigned int _innerSize,
+		const MappingCreator_t &_creator
+		)
+{
+	// first, create the spaces
+	NormedSpace_ptr_t X = NormedSpaceFactory::create(
+			_outerSize, _type_SpaceX, _args_SpaceX);
+	NormedSpace_ptr_t Y = NormedSpaceFactory::create(
+			_innerSize, _type_SpaceY, _args_SpaceY);
+
+	// then create the SpaceElement
+	SpaceElement_ptr_t y = ElementCreator::create(Y, _rhs);
+
+	// and the LinearMapping
+	Mapping_ptr_t A = _creator(X,Y);
+
+	InverseProblem_ptr_t instance(
+			new InverseProblem(A,X,Y,y) );
+
+	return instance;
+}
 
 InverseProblem_ptr_t InverseProblemFactory::create(
 		const std::string &_type_SpaceX,
@@ -24,21 +55,31 @@ InverseProblem_ptr_t InverseProblemFactory::create(
 		const Eigen::MatrixXd &_matrix,
 		const Eigen::VectorXd &_rhs)
 {
-	// first, create the spaces
-	NormedSpace_ptr_t X = NormedSpaceFactory::create(
-			_matrix.outerSize(), _type_SpaceX, _args_SpaceX);
-	NormedSpace_ptr_t Y = NormedSpaceFactory::create(
-			_matrix.innerSize(), _type_SpaceY, _args_SpaceY);
-
-	// then create the SpaceElement
-	SpaceElement_ptr_t y = ElementCreator::create(Y, _rhs);
-
-	// and the LinearMapping
-	Mapping_ptr_t A = LinearMappingFactory::createInstance(X,Y,_matrix);
-
-	InverseProblem_ptr_t instance(
-			new InverseProblem(A,X,Y,y) );
-
-	return instance;
+	const MappingCreator_t creator = boost::bind(&LinearMappingFactory::createInstance,
+			_1, _2, boost::cref(_matrix), false);
+	return create_internal(
+			_type_SpaceX, _args_SpaceX,
+			_type_SpaceY, _args_SpaceY,
+			_rhs,
+			_matrix.outerSize(), _matrix.innerSize(),
+			creator);
 }
 
+InverseProblem_ptr_t InverseProblemFactory::createFromTwoFactors(
+		const std::string &_type_SpaceX,
+		const args_t &_args_SpaceX,
+		const std::string &_type_SpaceY,
+		const args_t &_args_SpaceY,
+		const Eigen::MatrixXd &_matrix_first_factor,
+		const Eigen::MatrixXd &_matrix_second_factor,
+		const Eigen::VectorXd &_rhs)
+{
+	const MappingCreator_t creator = boost::bind(&LinearMappingFactory::createTwoFactorInstance,
+			_1, _2, boost::cref(_matrix_first_factor), boost::cref(_matrix_second_factor), false);
+	return create_internal(
+			_type_SpaceX, _args_SpaceX,
+			_type_SpaceY, _args_SpaceY,
+			_rhs,
+			_matrix_first_factor.innerSize(), _matrix_second_factor.outerSize(),
+			creator);
+}
--- src/Minimizations/InverseProblems/InverseProblemFactory.hpp
+++ src/Minimizations/InverseProblems/InverseProblemFactory.hpp
@@ -14,6 +14,7 @@
 #include <vector>
 
 #include <boost/any.hpp>
+#include <boost/function.hpp>
 
 #include <Eigen/Dense>
 
@@ -48,6 +49,57 @@ struct InverseProblemFactory
 			const args_t &_args_SpaceY,
 			const Eigen::MatrixXd &_matrix,
 			const Eigen::VectorXd &_rhs);
+
+	/** Creates an inverse problem in Lp spaces.
+	 *
+	 * @param  _type_SpaceX type of space for X
+	 * @param _args_SpaceX arguments required to create X
+	 * @param _type_SpaceY type of space for Y
+	 * @param _args_SpaceY arguments required to create Y
+	 * @param _matrix_first_factor first factor of the matrix as the
+	 * 			finite-dimensional representation of the linear mapping X -> Y
+	 * @param _matrix_second_factor first factor of the matrix as the
+	 * 			finite-dimensional representation of the linear mapping X -> Y
+	 * @param _rhs finite-dimensional representation of right-hand side.
+	 * @return created instance
+	 */
+	static InverseProblem_ptr_t createFromTwoFactors(
+			const std::string &_type_SpaceX,
+			const args_t &_args_SpaceX,
+			const std::string &_type_SpaceY,
+			const args_t &_args_SpaceY,
+			const Eigen::MatrixXd &_matrix_first_factor,
+			const Eigen::MatrixXd &_matrix_second_factor,
+			const Eigen::VectorXd &_rhs);
+
+private:
+	//!> typedef for the internal function to create the mapping
+	typedef boost::function<Mapping_ptr_t (NormedSpace_ptr_t, NormedSpace_ptr_t)> MappingCreator_t;
+
+	/** Internal function to create an inverse problem in Lp spaces via a given
+	 * mapping creator function.
+	 *
+	 * This just takes the common code from the two public create..() functions.
+	 *
+	 * @param  _type_SpaceX type of space for X
+	 * @param _args_SpaceX arguments required to create X
+	 * @param _type_SpaceY type of space for Y
+	 * @param _args_SpaceY arguments required to create Y
+	 * @param _rhs finite-dimensional representation of right-hand side.
+	 * @param _outerSize outer dimension of linear mapping
+	 * @param _innerSize outer dimension of linear mapping
+	 * @param _creator internal linear mapping creator function
+	 * @return created instance
+	 */
+	static InverseProblem_ptr_t create_internal(
+			const std::string &_type_SpaceX,
+			const args_t &_args_SpaceX,
+			const std::string &_type_SpaceY,
+			const args_t &_args_SpaceY,
+			const Eigen::VectorXd &_rhs,
+			const unsigned int _outerSize,
+			const unsigned int _innerSize,
+			const MappingCreator_t &_creator);
 };
 
 
