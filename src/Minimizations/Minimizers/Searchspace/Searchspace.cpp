8ddcdace256bc9c42b72216e376490a52d6fcb66
Frederik Heber <heber@ins.uni-bonn.de>
2017-04-11 14:29:56 +0200

    Added copyright notice to every module.
    - added AUTHORS and COPYING with GPL v2 license.

--- src/Minimizations/Minimizers/Searchspace/Searchspace.cpp
+++ src/Minimizations/Minimizers/Searchspace/Searchspace.cpp
@@ -1,4 +1,27 @@
 /*
+ * Project: BASSO - BAnach Sequential Subspace Optimizer
+ * Description: C++ library for solving optimization problems in Banach spaces
+ * Copyright (C)  2014-2016 University of Saarland. All rights reserved.
+ *
+ *
+ *   This file is part of the BASSO library.
+ *
+ *    BASSO is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    BASSO is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with BASSO.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
  * Searchspace.cpp
  *
  *  Created on: Nov 17, 2014
