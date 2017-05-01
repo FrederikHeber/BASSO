d1c128e3102d35f80c8cd26ebf04c2f66e005092
Frederik Heber <heber@ins.uni-bonn.de>
2017-05-01 23:05:43 +0200

    BregmanDistance uses getMinInf() when space is not smooth.

--- src/Minimizations/Functions/BregmanDistance.cpp
+++ src/Minimizations/Functions/BregmanDistance.cpp
@@ -16,7 +16,7 @@
 #include "Math/Helpers.hpp"
 #include "Minimizations/Elements/SpaceElement.hpp"
 #include "Minimizations/InverseProblems/InverseProblem.hpp"
-#include "Minimizations/Mappings/Mapping.hpp"
+#include "Minimizations/Mappings/DualityMapping.hpp"
 #include "Minimizations/Minimizers/MinimizationExceptions.hpp"
 #include "Minimizations/Norms/Norm.hpp"
 
@@ -59,12 +59,31 @@ double BregmanDistance::operator()(
 		const SpaceElement_ptr_t &_xdual
 		) const
 {
+	double resultSKP = 0.;
+	// check whether space is smooth
+	if (_x->getSpace()->getNorm()->isSmooth()) {
+		resultSKP =_xdual * _y;
+	} else {
+		// if not smooth, duality mapping is not single-value and hence we have
+		// to evaluate the infimum over all possible dual elements
+		const DualityMapping &mapping =
+				static_cast<DualityMapping &>(*_x->getSpace()->getDualityMapping());
+		SpaceElement_ptr_t xdual_mininf = _xdual->getSpace()->createElement();
+		mapping.getMinimumInfimum(_x, _y, xdual_mininf);
+		resultSKP = xdual_mininf * _y;
+	}
 	const double resultX = (1./Helpers::ConjugateValue(power)) * ::pow(norm(_x), power);
 	const double resultY = (1./power) * ::pow(norm(_y), power);
-	const double resultSKP =_xdual * _y;
 	double result = resultX+resultY-resultSKP;
 	LOG(debug, "Calculating Bregman distance between " << _x << " and " << _y
 			<< " as " << resultX << "+" << resultY << "-" << resultSKP << "="
 			<< result);
+	if (_x->getSpace()->getNorm()->isSmooth()) {
+		resultSKP =_xdual * _y;
+		double old_result = resultX+resultY-resultSKP;
+		LOG(debug, "Calculating former Bregman distance between " << _x << " and " << _y
+				<< " as " << resultX << "+" << resultY << "-" << resultSKP << "="
+				<< old_result);
+	}
 	return result;
 }
