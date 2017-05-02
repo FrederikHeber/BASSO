ed5d4fff46f6c9f46a19569585f2024dbb57f84d
Frederik Heber <frederik.heber@gmail.com>
2017-05-02 23:12:49 +0200

    tempcommit: Changes to RegularizedL1DualityMapping.#

--- src/Minimizations/Mappings/Specifics/RegularizedL1DualityMapping.cpp
+++ src/Minimizations/Mappings/Specifics/RegularizedL1DualityMapping.cpp
@@ -11,6 +11,7 @@
 
 #include <cmath>
 
+#include "Log/Logging.hpp"
 #include "Minimizations/Elements/ElementCreator.hpp"
 #include "Minimizations/Elements/SpaceElement.hpp"
 #include "Minimizations/Elements/RepresentationAdvocate.hpp"
@@ -39,17 +40,13 @@ void RegularizedL1DualityMapping::operator()(
 	// J=norm(x,1)^(q-1)*sign(x);
 	assert( getSourceSpace().get() == _x->getSpace().get() );
 	assert( getTargetSpace().get() == _Jx->getSpace().get() );
-	const L1Norm &l1norm = static_cast<L1Norm &>(*_x->getSpace()->getNorm());
 
-	const double factor = ::pow(l1norm.L1Norm::operator()(_x), (double)power-1.);
 	assert( _Jx->getSpace()->getDimension() == _x->getSpace()->getDimension() );
 	SpaceElement_ptr_t _Jx1 = getTargetSpace()->createElement();
-	const Eigen::VectorXd &vector = RepresentationAdvocate::get(_x);
-	RepresentationAdvocate::set(_Jx1,
-			factor * vector.array().cwiseProduct(
-					vector.array().abs().cwiseInverse()));
+	L1DualityMapping::operator()(_x, _Jx1);
+	LOG(debug, "Jx1 = " << (*_Jx1));
 	SpaceElement_ptr_t _Jx2 = getTargetSpace()->createElement();
-	RepresentationAdvocate::set(_Jx1, vector);
+	RepresentationAdvocate::set(_Jx2, RepresentationAdvocate::get(_x));
 	_Jx = _Jx1 + lambda * _Jx2;
 
 	// finish timing
@@ -72,24 +69,15 @@ void RegularizedL1DualityMapping::getMinimumInfimum(
 	// hence, for all intersections check whether flipped sign is better
 	assert( getSourceSpace().get() == _x->getSpace().get() );
 	assert( getTargetSpace().get() == _Jx->getSpace().get() );
-	const L1Norm &l1norm = static_cast<L1Norm &>(*_x->getSpace()->getNorm());
 
-	const double factor = ::pow(l1norm.L1Norm::operator()(_x), (double)power-1.);
 	assert( _Jx->getSpace()->getDimension() == _x->getSpace()->getDimension() );
 	SpaceElement_ptr_t _Jx1 = getTargetSpace()->createElement();
-	const Eigen::VectorXd &vector = RepresentationAdvocate::get(_x);
-	RepresentationAdvocate::set(_Jx1,
-			factor * vector.array().cwiseProduct(
-					vector.array().abs().cwiseInverse()));
-	for (unsigned int i=0;i<_x->getSpace()->getDimension();++i) {
-		if (fabs((*_x)[i]) < BASSOTOLERANCE ) {
-			if ((*_Jx1)[i] * (*_y)[i] > 0)
-				(*_Jx1)[i] *= -1.;
-		}
-	}
+	L1DualityMapping::getMinimumInfimum(_x, _y, _Jx1);
+//	LOG(debug, "Jx1 = " << (*_Jx1));
 	SpaceElement_ptr_t _Jx2 = getTargetSpace()->createElement();
-	RepresentationAdvocate::set(_Jx1, vector);
+	RepresentationAdvocate::set(_Jx2, RepresentationAdvocate::get(_x));
 	_Jx = _Jx1 + lambda * _Jx2;
+//	LOG(debug, "Jx = " << (*_Jx));
 
 	// finish timing
 	const boost::chrono::high_resolution_clock::time_point timing_end =
