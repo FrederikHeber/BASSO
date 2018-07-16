b7a6d2bc5b5d51e6a0b78e8846aa155a98e8e36e
Frederik Heber <frederik.heber@gmail.com>
2018-07-16 11:32:32 +0100

    Added "hello world" example from boost.python tutorial.
    - this shows that we can build the python module using cmake. Now, we only
      have to export the classes.

---
+++ src/Python/pybasso.cpp
@@ -0,0 +1,41 @@
+/*
+ * Project: BASSO - BAnach Sequential Subspace Optimizer
+ * Description: C++ library for solving optimization problems in Banach spaces
+ * Copyright (C)  2018 Frederik Heber. All rights reserved.
+ *
+ *
+ *   This file is part of the BASSO library.
+ *
+ *    BASSO is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    BASSO is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with BASSO.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* pybasso.cpp
+ *
+ *  Created on: Jul 16, 2018
+ *      Author: heber
+ */
+
+char const* greet()
+{
+   return "hello, world";
+}
+
+#include <boost/python.hpp>
+
+BOOST_PYTHON_MODULE(pyBasso)
+{
+    using namespace boost::python;
+    def("greet", greet);
+}
